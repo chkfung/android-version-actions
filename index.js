fcf66085155d848439fd4b0f40d63f9496b55578
Lam Fung <chkfung93@gmail.com>
2020-09-26 17:40:52 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+
+// versionCode — A positive integer [...] -> https://developer.android.com/studio/publish/versioning
+const versionCodeRegexPattern = /(versionCode(?:\s|=)*)(.*)/;
+// versionName — A string used as the version number shown to users [...] -> https://developer.android.com/studio/publish/versioning
+const versionNameRegexPattern = /(versionName(?:\s|=)*)(.*)/;
+
+try {
+    const gradlePath = core.getInput('gradlePath');
+    const versionCode = core.getInput('versionCode');
+    const versionName = core.getInput('versionName');
+    console.log(`Gradle Path : ${gradlePath}`);
+    console.log(`Version Code : ${versionCode}`);
+    console.log(`Version Name : ${versionName}`);
+
+    fs.readFile(gradlePath, 'utf8', function (err, data) {
+        newGradle = data;
+        if (versionCode.length > 0)
+            newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`);
+        if (versionName.length > 0)
+            newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${versionName}\"`);
+        fs.writeFile(gradlePath, newGradle, function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+            core.setOutput("result", `new version Code ${versionCode}`);
+            // Get the JSON webhook payload for the event that triggered the workflow
+            const payload = JSON.stringify(github.context.payload, undefined, 2)
+            console.log(`The event payload: ${payload}`);
+        });
+    });
+
+} catch (error) {
+    core.setFailed(error.message);
+}
